aacb1315972d578756821edbcbb093492029462f
bwjones91 <bwjones1991@gmail.com>
2018-08-14 18:41:44 -0400

    Dominant hand script added

--- Lumberjack RPG/Assets/Characters/Player/Player.cs
+++ Lumberjack RPG/Assets/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable {
 
@@ -12,7 +13,6 @@ public class Player : MonoBehaviour, IDamageable {
     [SerializeField] float maxAttackRange = 2f;
 
     [SerializeField] Weapon weaponInUse;
-    [SerializeField] GameObject weaponSocket;
 
     GameObject currentTarget;
     float currentHealthPoints;
@@ -31,9 +31,19 @@ public class Player : MonoBehaviour, IDamageable {
     private void PutWeaponInHand()
     {
         var weaponPrefab = weaponInUse.GetWeaponPrefab();
-        var weapon = Instantiate(weaponPrefab, weaponSocket.transform);
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
         weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
         weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on Player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+        return dominantHands[0].gameObject;
     }
 
     private void RegisterForMouseClick()
